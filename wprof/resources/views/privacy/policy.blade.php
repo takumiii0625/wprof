f0f76c811343d6439e24793c39b773c4ab5a92bc
takumiii0625 <takumitennis625@icloud.com>
2023-12-15 00:30:02 +0900

    プライバシーポリシー細字へ

--- wprof/resources/views/privacy/policy.blade.php
+++ wprof/resources/views/privacy/policy.blade.php
@@ -36,57 +36,57 @@
             <br>
             <hr>
             <section class="my-6">
-                <h2 class="font-bold">【１．事業者情報】</h2>
+                <h7 class="font-bold">【１．事業者情報】</h7>
                 <p>法人名：OBFall株式会社</p>
                 <p>所在地：東京都港区海岸１－２－３　汐留芝離宮ビルディング２１F</p>
                 <p>代表取締役：上遠野　博紀</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【２．個人情報の取得方法】</h2>
+                <h7 class="font-bold">【２．個人情報の取得方法】</h7>
                 <p>お問い合わせフォームの送信時には、氏名・会社名・電話番号・メールアドレスを取得させていただきます。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【３．個人情報の利用目的】</h2>
+                <h7 class="font-bold">【３．個人情報の利用目的】</h7>
                 <p>お問い合わせフォームで送信いただいた氏名・会社名・電話番号・メールアドレスは障害発生時に追跡のために使用させていただきます。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【４．個人データを安全に管理するための措置】</h2>
+                <h7 class="font-bold">【４．個人データを安全に管理するための措置】</h7>
                 <p>個人情報を正確かつ最新の内容に保つよう努めます。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【５．個人データの共同利用】</h2>
+                <h7 class="font-bold">【５．個人データの共同利用】</h7>
                 <p>当社は個人データの共同利用を行いません。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【６．個人データの第三者提供について】</h2>
+                <h7 class="font-bold">【６．個人データの第三者提供について】</h7>
                 <p>当社は法令及びガイドラインに別段の定めがある場合を除き、同意を得ないで第三者に個人情報を提供することは致しません。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【７．個人情報取り扱いに関する相談や苦情の連絡先】</h2>
+                <h7 class="font-bold">【７．個人情報取り扱いに関する相談や苦情の連絡先】</h7>
                 <p>当社の個人情報の取り扱いに関するご質問やご不明点、苦情、その他のお問い合わせはお問い合わせフォームよりご連絡ください。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【８．SSL（Secure Socket Layer）について】</h2>
+                <h7 class="font-bold">【８．SSL（Secure Socket Layer）について】</h7>
                 <p>当社のWebサイトはSSLに対応しており、WebブラウザとWebサーバーとの通信を暗号化しています。ユーザーが入力する氏名や、電話番号、メールアドレスなどの個人情報は自動的に暗号化されます。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【９．cookieについて】</h2>
+                <h7 class="font-bold">【９．cookieについて】</h7>
                 <p>cookieとは、WebサーバーからWebブラウザに送信されるデータのことです。Webサーバーがcookieを参照することでユーザーのパソコンを識別でき、効率的に当社Webサイトを利用することができます。当社Webサイトがcookieとして送るファイルは、個人を特定するような情報は含んでおりません。お使いのWebブラウザの設定により、cookieを無効にすることも可能です。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【１０．プライバシーポリシーの制定日及び改定日】</h2>
+                <h7 class="font-bold">【１０．プライバシーポリシーの制定日及び改定日】</h7>
                 <p>制定：2022年7月1日</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【１１．免責事項】</h2>
+                <h7 class="font-bold">【１１．免責事項】</h7>
                 <p>当社Webサイトに掲載されている情報の正確性には万全を期していますが、利用者が当社Webサイトの情報を用いて行う一切の行為に関して、一切の責任を負わないものとします。当社は、利用者が当社Webサイトを利用したことにより生じた利用者の損害及び利用者が第三者に与えた損害に関して、一切の責任を負わないものとします。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【１２．著作権・肖像権】</h2>
+                <h7 class="font-bold">【１２．著作権・肖像権】</h7>
                 <p>当社Webサイト内の文章や画像、すべてのコンテンツは著作権・肖像権等により保護されています。無断での使用や転用は禁止されています。</p>
             </section>
             <section class="my-6">
-                <h2 class="font-bold">【１３．リンク】</h2>
+                <h7 class="font-bold">【１３．リンク】</h7>
                 <p>当社Webサイトへのリンクは、自由に設置していただいて構いません。ただし、Webサイトの内容等によってはリンクの設置をお断りすることがあります。</p>
             </section>
         </div>
